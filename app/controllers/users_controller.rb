87c0af75ab117e2ce36f8916efdefa73bce2439f
Joe McPartland <joemcpartland@gmail.com>
2021-05-13 06:53:46 -0600

    added show users and index of users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,22 @@
 class UsersController < ApplicationController
+  # users index
+  get "/users" do
+    @users = User.all
+    erb :"users/index.html"
+  end
 
+  # users show
+  get "/users/:id" do
+    @user = User.find(params[:id])
+    erb :"users/show.html"
+  end
+
+  # new
   get "/users/new" do
-
     erb :"/users/new.html"
   end
 
+  # create
   post "/users" do
     user = User.create(params)
     session[:user_id] = user.id
